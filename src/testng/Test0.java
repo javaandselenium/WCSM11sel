d7dc691ae674b3fa9a8104380bbe0e267b8eef13
QSP <QSP@QSP>
2021-12-01 10:06:43 +0530

    TEstng

---
+++ src/testng/Test0.java
@@ -0,0 +1,15 @@
+package testng;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class Test0 {
+	
+@Test
+public void demo()
+{
+	Reporter.log("hello selenium",true);
+	Reporter.log("Hello java",false);
+}
+
+}
